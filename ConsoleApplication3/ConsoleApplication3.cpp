435eb7cbfdc9a5aeb1797467143e8b2882b7a7fb
Bohdan <Bohdan@DESKTOP-PIR5C7D>
2023-10-13 20:04:53 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApplication3/ConsoleApplication3.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+
+int main()
+{
+    int gryvni, copecks;
+
+    std::cout << "Enter the amount of gryvnas: ";
+    std::cin >> gryvni;
+
+    std::cout << "Enter the amount of copecks: ";
+    std::cin >> copecks;
+
+    gryvni = gryvni + copecks / 100;
+    copecks %= 100;
+
+    std::cout << gryvni << " gryvni\t" << copecks << " copecks";
+}
